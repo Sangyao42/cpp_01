69145154d801485075218b006aa9c13a60628621
Sangyao42 <wangsangyao@hotmail.com>
2023-08-03 21:34:44 +0200

    Finished[ex05]: learnt how to use pointers to member functions using typedef and define a macro CALL_MEMBER_FN / std::invoke

---
+++ ex05/src/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sawang <sawang@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/03 21:11:05 by sawang            #+#    #+#             */
+/*   Updated: 2023/08/03 21:30:01 by sawang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+#include <iostream>
+
+int	main(void)
+{
+	Harl harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	
+	harl.complain("TEST");
+	return (0);
+}
