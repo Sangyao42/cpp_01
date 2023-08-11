ed6fc58ea1165d1764dab5becb1478882b17d77d
Sangyao42 <wangsangyao@hotmail.com>
2023-08-11 17:03:36 +0200

    Added[Zombie]: default constructor

--- ex00/include/Zombie.hpp
+++ ex00/include/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: sawang <sawang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 18:49:03 by sawang            #+#    #+#             */
-/*   Updated: 2023/08/01 18:49:03 by sawang           ###   ########.fr       */
+/*   Updated: 2023/08/11 16:53:31 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 class Zombie
 {
 	public:
+		Zombie();
 		Zombie(std::string name);
 		~Zombie();
 
--- ex00/src/Zombie.cpp
+++ ex00/src/Zombie.cpp
@@ -6,11 +6,13 @@
 /*   By: sawang <sawang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 18:59:16 by sawang            #+#    #+#             */
-/*   Updated: 2023/08/01 18:59:16 by sawang           ###   ########.fr       */
+/*   Updated: 2023/08/11 16:53:36 by sawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+
+Zombie::Zombie() {}
 
 Zombie::Zombie(std::string name) : _name(name) {}
 
